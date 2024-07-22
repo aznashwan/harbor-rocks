13acb81a84e0a1dc0d0f26670947ca7eae4e1b40
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2024-07-23 00:21:02 +0300

    Add definitions for v2.9.3 images.
    Signed-off-by: Nashwan Azhari <nazhari@cloudbasesolutions.com>

--- tests/sanity/test_registry_photon.py
+++ tests/sanity/test_registry_photon.py
@@ -17,7 +17,7 @@ LOG.addHandler(logging.StreamHandler(sys.stdout))
 
 
 IMAGE_NAME = "registry-photon"
-IMAGE_VERSIONS = ["v2.10.2"]
+IMAGE_VERSIONS = ["v2.9.3", "v2.10.2"]
 
 
 @pytest.mark.abort_on_fail
