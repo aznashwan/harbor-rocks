7c682c1b25dfe1a87c3148a9297c43761d02af01
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2024-07-22 16:15:57 +0300

    Fetch testing architecture in integration test.
    Signed-off-by: Nashwan Azhari <nazhari@cloudbasesolutions.com>

--- tests/integration/test_harbor_rocks_v2_10_2.py
+++ tests/integration/test_harbor_rocks_v2_10_2.py
@@ -3,9 +3,12 @@
 # See LICENSE file for licensing details
 #
 
+import pytest
+
 from k8s_test_harness import harness
 from k8s_test_harness.util import env_util
 from k8s_test_harness.util import exec_util
+from k8s_test_harness.util import platform_util
 
 
 CHART_RELEASE_URL = (
@@ -32,19 +35,33 @@ IMAGE_NAMES_TO_CHART_VALUES_OVERRIDES_MAP = {
 def test_harbor_chart_deployment(module_instance: harness.Instance):
 
     version = "v2.10.2"
+    architecture = platform_util.get_current_rockcraft_platform_architecture()
 
     # Compose the Helm command line args for overriding the
     # image fields for each component:
     all_chart_value_overrides_args = []
-    for rmi in env_util.get_rocks_meta_info_from_env():
+    found_env_rocks_metadata = []
+    all_rocks_meta_info = env_util.get_rocks_meta_info_from_env()
+    for rmi in all_rocks_meta_info:
         if rmi.name in IMAGE_NAMES_TO_CHART_VALUES_OVERRIDES_MAP and (
-                rmi.arch == "amd64"):
+                rmi.version == version and rmi.arch == architecture):
             chart_section = IMAGE_NAMES_TO_CHART_VALUES_OVERRIDES_MAP[rmi.name]
             repo, tag = rmi.image.split(':')
             all_chart_value_overrides_args.extend([
                 "--set", f"{chart_section}.image.repository={repo}",
                 "--set", f"{chart_section}.image.version={tag}"
             ])
+            found_env_rocks_metadata.append(rmi.name)
+
+    missing = [
+        img
+        for img in IMAGE_NAMES_TO_CHART_VALUES_OVERRIDES_MAP
+        if img not in found_env_rocks_metadata]
+    if missing:
+        pytest.fail(
+            f"Failed to find built ROCK metadata for images {missing} "
+            f"of version '{version}' and architecture '{architecture}'. "
+            f"All built images metadata was: {all_rocks_meta_info}")
 
     helm_command = [
         "sudo",
